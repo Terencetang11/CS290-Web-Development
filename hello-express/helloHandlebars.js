9504695bda4079717f3ab63ad563058a9a10e43c
Terencetang11 <59594491+Terencetang11@users.noreply.github.com>
2020-07-27 17:52:41 -0700

    playing w/ sessions

--- hello-express/helloHandlebars.js
+++ hello-express/helloHandlebars.js
@@ -1,50 +1,65 @@
 var express = require('express');
-
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var session = require('express-session');
+app.use(session({secret: 'supersecretpassword'}));
+
+// render homepage
 app.get('/',function(req,res){
   res.render('home');
 });
 
+//renders other-page
 app.get('/other-page',function(req,res){
   res.render('other-page');
 });
 
+// renders counts page which maintains a sessions tracker for count of page visits by single browser instance
+app.get('counts', function(req,res){
+  var context = {}
+  context.counts = req.session.count || 0;
+  req.session.count = context.counts + 1;
+  res.render('counts', context);
+});
 
+
+// generates time as handlebars input object
 function genContext(){
   var stuffToDisplay = {};
   stuffToDisplay.time = (new Date(Date.now())).toLocaleTimeString('en-US');
   return stuffToDisplay;
 }
-
+// renders time page
 app.get('/time',function(req,res){
-  
   res.render('time', genContext());
   console.log(genContext())
 });
 
+// generates random int as handlebars input object
 function getRandomInt(max) {
   var stuffToDisplay = {};
   stuffToDisplay.num = Math.floor(Math.random() * Math.floor(max));
   return stuffToDisplay;
 }
-
+// renders random number page
 app.get('/random-number', function(req,res){
   var num = getRandomInt(1000);
   res.render('random-number', num);
   console.log(num)
 });
 
+// handles 404 status code issues
 app.use(function(req,res){
   res.status(404);
   res.render('404');
 });
 
+// handles 500 status code issues
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.type('plain/text');
@@ -52,6 +67,7 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
+// sets which web port to listen to for http requests
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
