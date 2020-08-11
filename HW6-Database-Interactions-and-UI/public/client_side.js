e74d2b87fab29f7806b672da99f162c17db75310
Terencetang11 <59594491+Terencetang11@users.noreply.github.com>
2020-08-11 06:14:40 -0700

    loads reset table page

--- HW6-Database-Interactions-and-UI/public/client_side.js
+++ HW6-Database-Interactions-and-UI/public/client_side.js
@@ -275,22 +275,21 @@ document.getElementById('reset_table').addEventListener('click', function(event)
     var payload = 'http://flip3.engr.oregonstate.edu:11179/reset-table'
     req.open("GET", payload, true);
     // set up listener prior to send to make aync call 
-    req.addEventListener('load',function(){
-        if(req.status >= 200 && req.status < 400){
-            var response = JSON.parse(req.responseText);
+    // req.addEventListener('load',function(){
+    //     if(req.status >= 200 && req.status < 400){
+    //         var response = JSON.parse(req.responseText);
             
-            // resets input form values
-            document.getElementById('name_input').value = null;
-            document.getElementById('reps_input').value = null;
-            document.getElementById('weight_input').value = null;
-            document.getElementById('date_input').value = null;
+    //         // resets input form values
+    //         document.getElementById('name_input').value = null;
+    //         document.getElementById('reps_input').value = null;
+    //         document.getElementById('weight_input').value = null;
+    //         document.getElementById('date_input').value = null;
             
-            // builds table from latest sql db updates
-            document.getElementById('resultsP').textContent = response.results;
-            buildTable([]);
-        } else {
-            console.log("Error in network request: " + req.statusText);
-        }});
+    //         // builds table from latest sql db updates
+    //         document.getElementById('resultsP').textContent = response.results;
+    //         buildTable([]);
+    //     } else {
+    //         console.log("Error in network request: " + req.statusText);
+    //     }});
     req.send(null);
-    event.preventDefault();
 })
