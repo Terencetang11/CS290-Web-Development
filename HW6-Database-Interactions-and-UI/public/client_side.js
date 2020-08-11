9ec3290c0acb708da16fd5a8a70b08569f7eb846
Terencetang11 <59594491+Terencetang11@users.noreply.github.com>
2020-08-10 23:56:17 -0700

    move up delete func

--- HW6-Database-Interactions-and-UI/public/client_side.js
+++ HW6-Database-Interactions-and-UI/public/client_side.js
@@ -39,6 +39,31 @@ function bindButtons(){
     })
 }  
 
+function deleteRow(tableID,currentRow) {
+    try {
+        var table = document.getElementById(tableID);
+        var rowCount = table.rows.length;
+        for (var i = 0; i < rowCount; i++) {
+            var row = table.rows[i];
+            /*var chkbox = row.cells[0].childNodes[0];*/
+            /*if (null != chkbox && true == chkbox.checked)*/
+            
+            if (row==currentRow.parentNode.parentNode) {
+                if (rowCount <= 1) {
+                    alert("Cannot delete all the rows.");
+                    break;
+                }
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+            }
+        }
+    } catch (e) {
+        alert(e);
+    }
+    //getValues();
+}
+
 function buildTable(rows){
     // Create new Table Node on each query pull
     element = document.getElementById("results_table");
@@ -140,27 +165,4 @@ function generateBorder(tagName){
 }
 
 
-function deleteRow(tableID,currentRow) {
-    try {
-        var table = document.getElementById(tableID);
-        var rowCount = table.rows.length;
-        for (var i = 0; i < rowCount; i++) {
-            var row = table.rows[i];
-            /*var chkbox = row.cells[0].childNodes[0];*/
-            /*if (null != chkbox && true == chkbox.checked)*/
-            
-            if (row==currentRow.parentNode.parentNode) {
-                if (rowCount <= 1) {
-                    alert("Cannot delete all the rows.");
-                    break;
-                }
-                table.deleteRow(i);
-                rowCount--;
-                i--;
-            }
-        }
-    } catch (e) {
-        alert(e);
-    }
-    //getValues();
-}
+
