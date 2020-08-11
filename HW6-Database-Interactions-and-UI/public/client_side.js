13399b3d77d3218200efaecb7fb31ae7ba50c438
Terencetang11 <59594491+Terencetang11@users.noreply.github.com>
2020-08-10 23:27:11 -0700

    console check

--- HW6-Database-Interactions-and-UI/public/client_side.js
+++ HW6-Database-Interactions-and-UI/public/client_side.js
@@ -30,7 +30,7 @@ function bindButtons(){
                 document.getElementById('unit_input').value = null;
                 
                 testRows = [{"id":1,"name":"test1","reps":0,"weight":0,"date":"0000-00-00","unit":""},{"id":2,"name":"test 2","reps":0,"weight":0,"date":"0000-00-00","unit":""},{"id":3,"name":"test 3","reps":0,"weight":0,"date":"0000-00-00","unit":""}]
-
+                console.log(testRows)
                 buildTable(testRows)
                 
             } else {
