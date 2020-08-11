4f400c7a7a7eb33f840aede08a5845f732c68c7f
Terencetang11 <59594491+Terencetang11@users.noreply.github.com>
2020-08-10 17:49:40 -0700

    updated sql server pw

--- express-mysql/dbcon.js
+++ express-mysql/dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql.eecs.oregonstate.edu',
+  host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs290_tangte',
   password        : '1042',
   database        : 'cs290_tangte'
